bb526b22f283a329ab2849a54d8e4787eb5efba2
Shaahin Yazarloo <shaahin2010@gmail.com>
2019-06-26 00:01:38 +0430

    initial commit

---
+++ src/test/java/com/gmail/shaahin2010/springcucumber/SpringCucumberApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gmail.shaahin2010.springcucumber;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCucumberApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
